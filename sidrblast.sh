cc25f21bc468a35eaedba43f50d3ebd80802a2e9
Victoria K Eggers <86014601+vkeggers@users.noreply.github.com>
2024-03-29 06:53:47 -0400

    Update sidrblast.sh

--- sidrblast.sh
+++ sidrblast.sh
@@ -20,7 +20,7 @@ blastn -query ${GENOME} \
    # -outfmt 6 \
     -culling_limit 5 \
     -evalue 1e-25 \
-    -out SIDRblast.txt
+    -out BLASToutput.txt
 
 
 ################################
@@ -31,7 +31,7 @@ blastn -query ${GENOME} \
 
 #blast results are by default sorted with the most significant e-value occurring first
 #get the first blast results for each contig into a list
-awk '/Query=/{print; flag=1; next} flag && /^>/{print; flag=0} flag && /No hits found/{print; flag=0}' out_blastnt.log > test1.txt
+awk '/Query=/{print; flag=1; next} flag && /^>/{print; flag=0} flag && /No hits found/{print; flag=0}' BLASToutput.txt > test1.txt
 awk 'NR%2==1{col1=$0} NR%2==0{print col1, $0}' test1.txt > test2.txt
 sed -i 's/PREDICTED: //' test2.txt
 sed -i 's/\*\*\*\*\* No hits found \*\*\*\*\*/> No No_hits_found/g' test2.txt 
@@ -42,7 +42,7 @@ sort -k1 test3.txt >> TopHitBLAST.txt
 
 
 #get the most frequent blast results into a list
-awk '/Query=/{print; flag=1; next} flag && /^>/{print; next} flag && /No hits found/{print; flag=0}' testData_flye_blast.txt > test1.txt
+awk '/Query=/{print; flag=1; next} flag && /^>/{print; next} flag && /No hits found/{print; flag=0}' BLASToutput.txt > test1.txt
 sed 's/PREDICTED: //' test1.txt > test2.txt
 awk '/Query=/{print; flag=1; next} flag && /^>/{print $1,$3; next} flag && /No hits found/{print; flag=0}' test2.txt > test3.txt
 sed 's/\*\*\*\*\* No hits found \*\*\*\*\*/> No_hits_found/g' test3.txt > test4.txt
@@ -57,3 +57,5 @@ rm test*.txt
 
 #join the two blast outputs together
 join --check-order --header -t$'\t' TopHitBLAST.txt MostFrequentBLAST.txt > SIDRblast.txt
+
+#I'm not sure that this is worth keeping because there were no instances when the most hit was Oscheius but the top hit wasn't
