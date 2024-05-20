5056f13fae62f5a32815774d3951389fa0f055cf
Victoria K Eggers <86014601+vkeggers@users.noreply.github.com>
2024-05-19 23:08:24 -0400

    Update plusANDminusCounts.sh

--- plusANDminusCounts.sh
+++ plusANDminusCounts.sh
@@ -59,6 +59,29 @@ echo -e "contig\tRNA_minus_counts" > RNA_minus_counts.txt
 samtools view -f 16 ./../samsANDbams/RNAaln_sorted.bam | awk '{if ($3 != contig) {if (contig != "") print contig, count; contig = $3; count = 0} count++} END {if (contig != "") print contig, count}' >> RNA_minus_counts.txt
 sed -i 's/ /\t/' RNA_minus_counts.txt
 
+cut -f 1 RNA_plus_counts.txt | sed '1d' | sort > names.temp
+lines_file1=$(wc -l < contig_names.txt)
+lines_file2=$(wc -l < names.temp)
+
+# Check if the number of lines match
+if [ "$lines_file1" -eq "$lines_file2" ]; then
+    echo "complete"
+else
+    echo "The number of lines do not match. Checking for 0 coverage."
+    grep -Fxv -f contig_names.txt names.temp > difference.temp
+    awk '{print $1, "0", "0"}' difference.temp | sed 's/ /\t/g' >> RNA_plus_counts.txt
+    sort RNA_plus_counts.txt > temp
+    mv temp RNA_plus_counts.txt
+
+    awk '{print $1, "0", "0"}' difference.temp | sed 's/ /\t/g' >> RNA_minus_counts.txt
+    sort RNA_minus_counts.txt > temp
+    mv temp RNA_minus_counts.txt
+    
+    rm difference.temp
+fi
+
+rm names.temp
+
 ############################################################################################################################################
 
 cd ..
