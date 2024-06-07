1dd055448cb3c20708c862cde5be571c637179ef
Daniel Morales <79028039+dmora127@users.noreply.github.com>
2024-06-06 20:38:05 -0400

    updated xgboost_renamed.py

--- xgboost_renamed.py
+++ xgboost_renamed.py
@@ -24,58 +24,58 @@ parser = argparse.ArgumentParser(description='XGBoost model to predict the conta
 parser.add_argument('--Genus', type=str, help='Genus of the the species of interest')
 args = parser.parse_args()
 genus = args.Genus
+biasedOutput = pd.DataFrame(columns=[genus, 'Other'])
 
+genus = 'Caenorhabditis'
 
+for i in range(100):
 
-# Make a new dataframe titled biasedOutput with columns 'Oscheius' and 'Other'
-biasedOutput = pd.DataFrame(columns=['Oscheius', 'Other'])
+    # Load SIDR stats tsv file to a pandas dataframe titled stats
+    stats = pd.read_csv('./data/SIDRstats.tsv', sep='\t', header=0)
 
-for i in range(10000):
+    # Make stats['Origin'] = 1 if stats['TopHit'] or stats['MostHits'] contain the genus name, else 0
+    stats['Origin'] = np.where(stats['TopHit'].str.contains(genus, na=False) | stats['MostHits'].str.contains(genus, na=False), 1, 0)
 
-    # Load SIDR stats tsv file to a pandas dataframe titled data
-    stats = pd.read_csv('./SIDR/data/SIDRstats.tsv', sep='\t', header=0)
-
-    # TODO: Make stats['Origin'] = TRUE if stats['TopHit'] or stats['MostHits'] contain the genus name
-    # For each row in stats, if the 'TopHit' or 'MostHits' columns contain the genus name, then change the 'Origin' column to 1, else 0
-    stats['Origin'] = np.where(stats['TopHit'].str.contains(genus) or stats['MostHits'].str.contains(genus), 1, 0)
-
-    # Make a new dataframe titled trainingDF that only contains rows which do not equal 'No hits found' in the 'Origin' column
+    # Make a new dataframe titled trainingDF that only contains rows which do not equal 'No hits found' in the 'TopHit' column
     trainingDF = stats[stats['TopHit'] != 'No hits found']
 
-    # Make a new dataframe titled orig where if the 'Origin' column equals the 'Genus' string variable. If it is True then change the Origin value to True, else False
-    orig = trainingDF[['Origin']]
-    orig['Origin'] = trainingDF['Origin'].str.contains(genus).astype(int)
+    # Drop the 'TopHit' and 'MostHits' columns from the trainingDF dataframe
+    trainingDF = trainingDF.drop(['TopHit', 'MostHits'], axis=1)
 
-    # Make a variable titled "Train" that samples trainingDF with a random sampling 1/3 of the data in the dataframe
-    Train = trainingDF.sample(frac=1/3)
+    # Create a variable titled "Train" that samples trainingDF with a random sampling 1/3 of the data in the dataframe
+    Train = trainingDF.sample(frac=1/3, random_state=i)
 
-    # Make a variable titled stat1_test which takes all the rows not in Train and keeps all columns except the 'contig' and 'Origin' columns
-    stat1_test = trainingDF[~trainingDF.index.isin(Train.index)].drop(['contig', 'Origin'], axis=1)
-
+    # Create training and testing datasets by separating based on the sampled indices
     stat1_train = trainingDF[trainingDF.index.isin(Train.index)].drop(['contig', 'Origin'], axis=1)
+    stat1_test = trainingDF[~trainingDF.index.isin(Train.index)].drop(['contig', 'Origin'], axis=1)
+    origin_train = trainingDF[trainingDF.index.isin(Train.index)]['Origin']
+    origin_test = trainingDF[~trainingDF.index.isin(Train.index)]['Origin']
 
-    origin_test = orig[~orig.index.isin(Train.index)]
+    # Calculate value counts for the 'Origin' column
+    value_counts = origin_train.value_counts()
 
-    origin_train = orig[orig.index.isin(Train.index)]
+    # Ensure the values for 'Caenorhabditis' and 'Other' are present in value_counts
+    caeno_count = value_counts.get(1, 0)
+    other_count = value_counts.get(0, 0)
 
-    origin_train['Origin'].value_counts()
+    # Append values to biasedOutput dataframe
+    new_row = pd.DataFrame({genus: [caeno_count], 'Other': [other_count]})
+    biasedOutput = pd.concat([biasedOutput, new_row], ignore_index=True)
 
-    # append values to biasedOutput dataframe where the number of 1's goes in the 'Oscheius' column and the number of 0's goes in the 'Other' column
-    biasedOutput = biasedOutput.append({'Oscheius': origin_train['Origin'].value_counts()[1], 'Other': origin_train['Origin'].value_counts()[0]}, ignore_index=True)
 # Save the biasedOutput dataframe to a new tsv file
-biasedOutput.to_csv('./SIDR/results/biasedOutput.tsv', sep='\t', index=False)
+biasedOutput.to_csv('./results/biasedOutput.tsv', sep='\t', index=False)
 
 # Use biasedOutput dataframe to make a simple box and whisker plot
-# set data in the biasedOutput dataframe to numeric
+# Set data in the biasedOutput dataframe to numeric
 biasedOutput = biasedOutput.apply(pd.to_numeric)
 
 # Create the figure and two subplots (axes) with shared x-axes
 fig, (ax, ax2) = plt.subplots(2, 1, sharex=True, figsize=(10, 8))
-fig.subplots_adjust(hspace=0.1)  # adjust space between axes
+fig.subplots_adjust(hspace=0.1)  # Adjust space between axes
 
 # Plotting the data - vertical box plots
-ax.boxplot([biasedOutput['Oscheius'], biasedOutput['Other']], labels=['Oscheius', 'Other'])
-ax2.boxplot([biasedOutput['Oscheius'], biasedOutput['Other']], labels=['Oscheius', 'Other'])
+ax.boxplot([biasedOutput[genus], biasedOutput['Other']], labels=[genus, 'Other'])
+ax2.boxplot([biasedOutput[genus], biasedOutput['Other']], labels=[genus, 'Other'])
 
 # Define upper and lower range for the y-axis on both plots to zoom into different data ranges
 ax.set_ylim(480, 530)  # Set this range to capture outlier or higher value ranges
@@ -85,25 +85,25 @@ ax2.set_ylim(0, 45)    # Set this range to capture most of the data
 ax.spines['bottom'].set_visible(False)
 ax2.spines['top'].set_visible(False)
 ax.xaxis.tick_top()
-ax.tick_params(labeltop=False)  # don't put tick labels at the top
+ax.tick_params(labeltop=False)  # Don't put tick labels at the top
 ax2.xaxis.tick_bottom()
 
 # Diagonal lines to indicate the break in the plot
-d = .015  # diagonal line length
+d = .015  # Diagonal line length
 kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
-ax.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
-ax.plot((1 - d, 1 + d), (-d, +d), **kwargs)  # top-right diagonal
+ax.plot((-d, +d), (-d, +d), **kwargs)        # Top-left diagonal
+ax.plot((1 - d, 1 + d), (-d, +d), **kwargs)  # Top-right diagonal
 
-kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
-ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # bottom-left diagonal
-ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # bottom-right diagonal
+kwargs.update(transform=ax2.transAxes)  # Switch to the bottom axes
+ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # Bottom-left diagonal
+ax2.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  # Bottom-right diagonal
 
 # Add a main title to the figure
-fig.suptitle('Box and Whisker Plot of Oscheius and Other Origin\nCounts with 10,000 Replicates', fontsize=16)
+fig.suptitle('Box and Whisker Plot of Caenorhabditis and Other Origin\nCounts with 10 Replicates', fontsize=16)
 
 # Show the plot
 plt.show()
-plt.savefig('./SIDR/figures/TrainDataBalancePlot.png', dpi=1200, bbox_inches='tight')
+plt.savefig('./figures/TrainDataBalancePlot.png', dpi=1200, bbox_inches='tight')
 plt.close()
 
 ####################
@@ -122,20 +122,20 @@ print("Accuracy: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
 model.save_model("model-1.json")
 
 # Print SIDR predictions to stats dataframe
-stats['SIDR_predictions'] = model.predict(stats.drop(['contig', 'Origin'], axis=1))
+stats['SIDR_predictions'] = model.predict(stats.drop(['contig', 'TopHit', 'MostHits', 'Origin'], axis=1))
 
 # Save the stats dataframe to a new tsv file
-stats.to_csv('./SIDR/results/Full_SIDR_predictions.tsv', sep='\t', index=False)
+stats.to_csv('./results/Full_SIDR_predictions.tsv', sep='\t', index=False)
 
 # Save the stats dataframe to a new tsv file if the SIDR_predictions column equals 1
-stats[stats['SIDR_predictions'] == 1].to_csv('./SIDR/results/Kept_SIDR_predictions.tsv', sep='\t', index=False)
-stats[stats['SIDR_predictions'] == 0].to_csv('./SIDR/results/Removed_SIDR_predictions.tsv', sep='\t', index=False)
+stats[stats['SIDR_predictions'] == 1].to_csv('./results/Kept_SIDR_predictions.tsv', sep='\t', index=False)
+stats[stats['SIDR_predictions'] == 0].to_csv('./results/Removed_SIDR_predictions.tsv', sep='\t', index=False)
 
 # TODO: Remove from graphviz - doesn't work
 # Plot the decision tree using matplotlib
 fig, ax = plt.subplots(figsize=(30, 30))
 xgboost.plot_tree(model, num_trees=0, ax=ax)
-plt.savefig('./SIDR/figures/tree_plot.png', dpi=1200, bbox_inches='tight')
+plt.savefig('./figures/tree_plot.png', dpi=1200, bbox_inches='tight')
 plt.close()
 
 # Feature Importance
@@ -150,7 +150,7 @@ report = classification_report(origin_test, y_pred)
 
 # Show the plot
 plt.show()
-plt.savefig('./SIDR/figures/feature_importance.png', dpi=1200, bbox_inches='tight')
+plt.savefig('./figures/feature_importance.png', dpi=1200, bbox_inches='tight')
 plt.close()
 
 
@@ -173,7 +173,7 @@ plt.ylabel('True Positive Rate')
 plt.title('Receiver Operating Characteristic Curve')
 plt.legend(loc="lower right")
 plt.show()
-plt.savefig('./SIDR/figures/ROCcurve.png', dpi=1200, bbox_inches='tight')
+plt.savefig('./figures/ROCcurve.png', dpi=1200, bbox_inches='tight')
 
 
 # Get predicted probabilities for positive class for training and test sets
@@ -204,4 +204,4 @@ plt.ylabel('True Positive Rate')
 plt.title('Receiver Operating Characteristic Curve')
 plt.legend(loc="lower right")
 plt.show()
-plt.savefig('./SIDR/figures/JointROCcurve.png', dpi=1200, bbox_inches='tight')
+plt.savefig('./figures/JointROCcurve.png', dpi=1200, bbox_inches='tight')
