c973a6df9763593fe8bfb577583f0240dc05c52c
Victoria K Eggers <86014601+vkeggers@users.noreply.github.com>
2024-09-04 15:41:36 -0400

    Update read_GC.sh

--- read_GC.sh
+++ read_GC.sh
@@ -1,123 +1,98 @@
 #!/bin/bash
-
+#SBATCH --account iacc_jfierst
+#SBATCH --qos highmem1
+#SBATCH --partition highmem1
+#SBATCH --output=out_read_GC.log
+#SBATCH --mail-user=vegge003@fiu.edu
 
 
 #calculate average GC content of the reads spanning the contig
 
-
-#load modules
-module load bedtools2-2.27.1-gcc-8.2.0-bxmhnwb
-module load samtools-1.15.1-gcc-8.2.0
-
-
-cd stats
+read_GC_start=$(date +%s)
 
 ########################################################################################################################################################
 
 #pacbio hifi
-
-mkdir PBgc_per_contig
-cd PBgc_per_contig
+if [ -n "${HIFI_READS}" ]; then
 
 #titles for columns in the output file
-echo -e "contig\tPBread_GC" > PBread_GC.txt
+    echo -e "contig\tread_GC_hifi" > ./stats/read_GC_hifi.txt
 
-#using bedtools, make a bed file showing the beginning and end of each read and which contig it belongs to
-bedtools bamtobed -i ./../../samsANDbams/PBaln_sorted.bam > output.bed
 
-#loop through each contig in contig_names.txt
-while read -r contig;
-    do
-
-        #grep all the reads belonging to the current contig and put those in a temporary bed file. We are making a subset of the original bed file
-        grep "${contig}" output.bed > "${contig}"_output.bed
-
-        #give us back the sequences for these reads by taking the subset bed file and making a bam file of it
-        #-L means to only output alignments that overlap with the region in the bed file and -h means to include headers | shuttle that sam output to a bam
-        samtools view -L "${contig}"_output.bed -h ./../../samsANDbams/PBaln_sorted.bam | samtools view -b - > "${contig}"reads.bam
-
-        #create a subset fastq from the subset bam, because I don't know how to work with sams and bams
-        bedtools bamtofastq -i "${contig}"reads.bam -fq "${contig}"reads.fastq
-
+#loop through each line in contig_names.txt
+    while read -r line; do
         #use awk to print the second of four lines (2nd line is sequence) in the subset fastq | substitute any G or C or g or c to an empty character and
         #count the number of substitutions. Save this count to a variable called gc_number
-        gc_number=$(awk 'NR%4 == 2 {print}' "${contig}"reads.fastq | awk '{count += gsub(/[gcGC]/, "");} END {print count}')
+        gc_number=$(awk 'NR%4 == 2 {print}' hifi_fastqs/"${line}".fastq | awk '{count += gsub(/[gcGC]/, "");} END {print count}')
 
         #use awk to print the second of four lines (2nd line is sequence) in the subset fastq | remove newline characters | count all characters and
         #save to a variable called total bases
-        total_bases=$(awk 'NR%4 == 2 {print}' "${contig}"reads.fastq | tr -d '\n' | wc -c)
+        total_bases=$(awk 'NR%4 == 2 {print}' hifi_fastqs/"${line}".fastq | tr -d '\n' | wc -c)
 
         #pass the variables to awk, divide gc_number by total_bases and multiply by 100
         gc_percent=$(awk -v total="$total_bases" -v value="$gc_number" 'BEGIN { printf((value/total)*100); }')
 
         #print and append the values for the current contig to the output file
-        echo -e "$contig\t$gc_content" >> PBread_GC.txt
+        echo -e "$line\t$gc_percent" >> ./stats/read_GC_hifi.txt
 
-        #remove subset files
-        rm "${contig}"_output.bed
-        rm "${contig}"reads.bam
-        rm "${contig}"reads.fastq
-    done < ./../contig_names.txt
-cp PBread_GC.txt ./../.
-cd ..
+    done < contig_names.txt
+
+fi
 
 
 ###################################################################################################################################################
 
 #Oxford Nanopore
+if [ -n "${ONT_READS}" ]; then
 
-mkdir ONTgc_per_contig
-cd ONTgc_per_contig
+    echo -e "contig\tread_GC_ont" > ./stats/read_GC_ont.txt
 
-echo -e "contig\tONTread_GC" > ONTread_GC.txt
-
-bedtools bamtobed -i ./../../samsANDbams/ONTaln_sorted.bam > output.bed
-
-while read -r contig;
-    do
-        grep "${contig}" output.bed > "${contig}"_output.bed
-        samtools view -L "${contig}"_output.bed -h ./../../samsANDbams/ONTaln_sorted.bam | samtools view -b - > "${contig}"reads.bam
-        bedtools bamtofastq -i "${contig}"reads.bam -fq "${contig}"reads.fastq
-        gc_number=$(awk 'NR%4 == 2 {print}' "${contig}"reads.fastq | awk '{count += gsub(/[gcGC]/, "");} END {print count}')
-        total_bases=$(awk 'NR%4 == 2 {print}' "${contig}"reads.fastq | tr -d '\n' | wc -c)
+    while read -r line; do
+        gc_number=$(awk 'NR%4 == 2 {print}' ont_fastqs/"${line}".fastq | awk '{count += gsub(/[gcGC]/, "");} END {print count}')
+        total_bases=$(awk 'NR%4 == 2 {print}' ont_fastqs/"${line}".fastq | tr -d '\n' | wc -c)
         gc_percent=$(awk -v total="$total_bases" -v value="$gc_number" 'BEGIN { printf((value/total)*100); }')
-        echo -e "$contig\t$gc_percent" >> ONTread_GC.txt
-        rm "${contig}"_output.bed
-        rm "${contig}"reads.bam
-        rm "${contig}"reads.fastq
-    done < ./../contig_names.txt
-cp ONTread_GC.txt ./../.
-cd ..
+        echo -e "$line\t$gc_percent" >> ./stats/read_GC_ont.txt
+    done < contig_names.txt
+
+fi
 
 
 ##################################################################################################################################################
 
 #RNA
+if [ -n "${RNA_FORWARD}" ]; then
 
-mkdir RNAgc_per_contig
-cd RNAgc_per_contig
+    echo -e "contig\tread_GC_rna" > ./stats/read_GC_rna.txt
 
-echo -e "contig\tRNAread_GC" > RNAread_GC.txt
-
-bedtools bamtobed -i ./../../samsANDbams/RNAaln_sorted.bam > output.bed
-
-while read -r contig;
-    do
-        grep "${contig}" output.bed > "${contig}"_output.bed
-        samtools view -L "${contig}"_output.bed -h ./../../samsANDbams/RNAaln_sorted.bam | samtools view -b - > "${contig}"reads.bam
-        bedtools bamtofastq -i "${contig}"reads.bam -fq "${contig}"reads.fastq
-        gc_number=$(awk 'NR%4 == 2 {print}' "${contig}"reads.fastq | awk '{count += gsub(/[gcGC]/, "");} END {print count}')
-        total_bases=$(awk 'NR%4 == 2 {print}' "${contig}"reads.fastq | tr -d '\n' | wc -c)
+    while read -r line; do
+        gc_number=$(awk 'NR%4 == 2 {print}' rna_fastqs/"${line}".fastq | awk '{count += gsub(/[gcGC]/, "");} END {print count}')
+        total_bases=$(awk 'NR%4 == 2 {print}' rna_fastqs/"${line}".fastq | tr -d '\n' | wc -c)
         gc_percent=$(awk -v total="$total_bases" -v value="$gc_number" 'BEGIN { printf((value/total)*100); }')
-        echo -e "$contig\t$gc_percent" >> RNAread_GC.txt
-        rm "${contig}"_output.bed
-        rm "${contig}"reads.bam
-        rm "${contig}"reads.fastq
-    done < ./../contig_names.txt
-cp RNAread_GC.txt ./../.
-cd ..
+        echo -e "$line\t$gc_percent" >> ./stats/read_GC_rna.txt
+    done < contig_names.txt
 
+fi
 
 ###################################################################################################################################################
 
-cd ..
+#DNA_Illumina
+if [ -n "${DNA_FORWARD}" ]; then
+
+    echo -e "contig\tread_GC_DNA_Illumina" > ./stats/read_GC_DNA_Illumina.txt
+
+    while read -r line; do
+        gc_number=$(awk 'NR%4 == 2 {print}' DNA_Illumina_fastqs/"${line}".fastq | awk '{count += gsub(/[gcGC]/, "");} END {print count}')
+        total_bases=$(awk 'NR%4 == 2 {print}' DNA_Illumina_fastqs/"${line}".fastq | tr -d '\n' | wc -c)
+        gc_percent=$(awk -v total="$total_bases" -v value="$gc_number" 'BEGIN { printf((value/total)*100); }')
+        echo -e "$line\t$gc_percent" >> ./stats/read_GC_DNA_Illumina.txt
+    done < contig_names.txt
+
+fi
+
+###################################################################################################################################################
+
+
+read_GC_end=$(date +%s)
+read_GC_runtime=$((read_GC_end - read_GC_start))
+read_GC_runtime_minutes=$((read_GC_runtime / 60))
+echo "read_GC stats completed in $read_GC_runtime_minutes minutes" >> progress.log
