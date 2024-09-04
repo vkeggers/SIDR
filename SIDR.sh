e545dcc14b55d2ec9103cf23cf419a11db03aef6
Victoria K Eggers <86014601+vkeggers@users.noreply.github.com>
2024-09-04 15:38:35 -0400

    Update SIDR.sh

--- SIDR.sh
+++ SIDR.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-
 #SBATCH --account iacc_jfierst
 #SBATCH --qos highmem1
 #SBATCH --partition highmem1
-#SBATCH -n 12
-#SBATCH --output=out_sidr.log
+#SBATCH --output=out_alignments.log
 #SBATCH --mail-user=vegge003@fiu.edu
-#SBATCH --mail-type=ALL
+#SBATCH -n 12
 
+export total_start=$(date +%s)
+touch progress.log
 
-############################
-############################
-########LOAD_MODULES########
-############################
-############################
+export GENOME=/scratch/jfierst/tori/SIDR/testData/pooja/ONTtestData02_flye_assembly_output.fasta
+export ONT_READS=/scratch/jfierst/tori/SIDR/testData/pooja/ONTtestData02.fastq
+export RNA_FORWARD=/scratch/jfierst/tori/SIDR/testData/pooja/Illumina_RNA_testData_F.fastq
+export RNA_REVERSE=/scratch/jfierst/tori/SIDR/testData/pooja/Illumina_RNA_testData_R.fastq
+export DNA_FORWARD=
+export DNA_REVERSE=
+export HIFI_READS=
+export NT=/home/data/jfierst/veggers/nt_db/nt
+export SAMPLE=testData
 
 module load samtools-1.15.1-gcc-8.2.0
 module load blast-plus-2.7.1-gcc-8.2.0-fm5yf3k
@@ -21,233 +25,362 @@ module load minimap2-2.24
 module load bwa-0.7.17-gcc-8.2.0-qgdird7
 module load bedtools2-2.27.1-gcc-8.2.0-bxmhnwb
 
+mkdir -p stats
 
-#############################
-#############################
-#######ARGUMENT_CENTER#######
-#############################
-#############################
-
-# Initialize variables for arguments
-FORWARD=""
-REVERSE=""
-GENOME=""
-HIFI_READS=""
-ONT_READS=""
-NT=""
-RUN_BLAST=""
-BLAST_OUTPUT=""
-GENUS=""
-
-# Help message with descriptions of all arguments
-print_help() {
-    echo "Usage: $0 [options]"
-    echo "Options:"
-    echo "  --forward/-f       A path to your RNA seq forward reads."
-    echo "  --reverse/-r       A path to your RNA seq reverse reads."
-    echo "  --genome/-g        A path to your genome assembly as a .fa file."
-    echo "  --genus/-x         *REQUIRED* The genus name of your species of interest (Case- and spelling-sensitive; Ex: -g Drosophila)."
-    echo "  --hifi             A path to your PacBio HiFi reads."
-    echo "  --ont              A path to your Oxford Nanopore basecalled reads."
-    echo "  --nt/-n            A path to your NCBI compiled NT database."
-    echo "  --blast/-b         *REQUIRED* Do you need to run a blast search of your assembly? Yes/No [Yes (if not completed)/ No (previously completed)]"
-    echo "  --blastoutput      Path to your blast output file."
-    echo "  --help/-h          Display this help and exit."
+print_elapsed_time() {
+    local start_time=$1
+    local end_time=$(date +%s)
+    local elapsed=$(( end_time - start_time ))
+    local elapsed_minutes=$(elapsed/60)
+    echo -e "Elapsed time: $elapsed minutes \n"
 }
 
-# Check if no arguments were provided
-if [ $# -eq 0 ]; then
-    print_help
-    exit 1
+
+samsANDbams_start=$(date +%s)
+
+grep ">" $GENOME | sed 's/>//g' > contig_names.txt
+
+mkdir contigs
+awk '/^>/{filename=sprintf("%s/%s.fasta", "contigs", substr($0,2)); next} {print > filename}' $GENOME
+
+mkdir samsANDbams
+
+if [ -n "${HIFI_READS}" ]; then
+
+        start_time_1=$(date +%s)
+
+        mkdir hifi_fastqs
+        cd samsANDbams
+        mkdir hifi_alignments
+        cd hifi_alignments
+
+        minimap2 -ax map-hifi ${GENOME} ${HIFI_READS} -o HIFIaln.sam
+        samtools view -Sb ./HIFIaln.sam -o ./HIFIaln.bam
+        samtools sort -o ./HIFIaln_sorted.bam ./HIFIaln.bam
+        samtools index ./HIFIaln_sorted.bam ./HIFIaln_sorted.bai
+        bedtools bamtobed -i ./HIFIaln_sorted.bam > output.bed
+
+        max_jobs=12
+        job_count=0
+
+        while read -r line; do
+                grep "${line}" output.bed > "${line}"_output.bed &
+
+                job_count=$((job_count + 1))
+
+                if [ "$job_count" -ge "$max_jobs" ]; then
+                        wait
+                        job_count=0
+                fi
+
+        done < ./../../contig_names.txt
+        wait
+
+        echo "Bed files for hifi reads done" >> ./../../progress.log
+        print_elapsed_time $start_time_1 >> ./../../progress.log
+
+
+        start_time_2=$(date +%s)
+        while read -r line; do
+
+                samtools view -L "${line}"_output.bed -h HIFIaln_sorted.bam | samtools view -b - > "${line}"_reads.bam &
+
+                job_count=$((job_count + 1))
+
+                if [ "$job_count" -ge "$max_jobs" ]; then
+                        wait
+                        job_count=0
+                fi
+
+        done < ./../../contig_names.txt
+
+        wait
+        echo "Bam files for hifi reads done" >> ./../../progress.log
+        print_elapsed_time $start_time_2 >> ./../../progress.log
+
+        rm *.bed
+
+
+        start_time_3=$(date +%s)
+        while read -r line; do
+
+                bedtools bamtofastq -i "${line}"_reads.bam -fq ./../../hifi_fastqs/"${line}".fastq &
+
+                job_count=$((job_count + 1))
+
+                if [ "$job_count" -ge "$max_jobs" ]; then
+                        wait
+                        job_count=0
+                fi
+
+        done < ./../../contig_names.txt
+
+        wait
+
+        echo "Fastq files for hifi reads done" >> ./../../progress.log
+        print_elapsed_time $start_time_3 >> ./../../progress.log
+
+        cd ..
+        cd ..
 fi
 
-# Loop through arguments and process them
-while [ "$1" != "" ]; do
-    case $1 in
-        -f | --forward )     shift
-                             FORWARD=$1
-                             ;;
-        -r | --reverse )     shift
-                             REVERSE=$1
-                             ;;
-        -g | --genome )      shift
-                             GENOME=$1
-                             ;;
-        -x | --genus )		 shift
-                             GENUS=$1
-                             ;;
-        --hifi )             shift
-                             HIFI_READS=$1
-                             ;;
-        --ont )              shift
-                             ONT_READS=$1
-                             ;;
-        -n | --nt )          shift
-                             NT=$1
-                             ;;
-        -b | --blast )       shift
-                             RUN_BLAST=$1
-                             if [ "$RUN_BLAST" != "Yes" ] && [ "$RUN_BLAST" != "No" ]; then
-                                 echo "Invalid argument for --blast/-b: $RUN_BLAST"
-                                 print_help
-                                 exit 1
-                             fi
-                             ;;
-        --blastoutput )      shift
-                             BLAST_OUTPUT=$1
-                             ;;
-        -h | --help )        print_help
-                             exit
-                             ;;
-        * )                  echo "Invalid argument: $1"
-                             print_help
-                             exit 1
-    esac
-    shift
-done
+wait
 
-# Check if --genus/-x was provided
-if [ -z "$GENUS" ]; then
-    echo "Error: --genus/-x argument is required."
-    print_help
-    exit 1
+if [ -n "${ONT_READS}" ]; then
+
+        start_time_4=$(date +%s)
+
+        mkdir ont_fastqs
+        cd samsANDbams
+        mkdir ont_alignments
+        cd ont_alignments
+
+        minimap2 -ax map-ont ${GENOME} ${ONT_READS} -o ONTaln.sam
+        samtools view -Sb ./ONTaln.sam -o ./ONTaln.bam
+        samtools sort -o ./ONTaln_sorted.bam ./ONTaln.bam
+        samtools index ./ONTaln_sorted.bam ./ONTaln_sorted.bai
+        bedtools bamtobed -i ./ONTaln_sorted.bam > output.bed
+
+        max_jobs=12
+        job_count=0
+         while read -r line; do
+                grep "${line}" output.bed > "${line}"_output.bed &
+
+                job_count=$((job_count + 1))
+
+                if [ "$job_count" -ge "$max_jobs" ]; then
+                        wait
+                        job_count=0
+                fi
+
+        done < ./../../contig_names.txt
+
+        wait
+
+        echo "Bed files for ont reads done" >> ./../../progress.log
+        print_elapsed_time $start_time_4 >> ./../../progress.log
+
+        start_time_5=$(date +%s)
+        while read -r line; do
+                samtools view -L "${line}"_output.bed -h ONTaln_sorted.bam | samtools view -b - > "${line}"_reads.bam &
+
+                job_count=$((job_count + 1))
+
+                if [ "$job_count" -ge "$max_jobs" ]; then
+                        wait
+                        job_count=0
+                fi
+
+        done < ./../../contig_names.txt
+
+        wait
+
+        echo "Bam files for ont reads done" >> ./../../progress.log
+        print_elapsed_time $start_time_5 >> ./../../progress.log
+
+        rm *.bed
+
+
+        start_time_6=$(date +%s)
+        while read -r line; do
+                bedtools bamtofastq -i "${line}"_reads.bam -fq ./../../ont_fastqs/"${line}".fastq &
+
+                job_count=$((job_count + 1))
+
+                if [ "$job_count" -ge "$max_jobs" ]; then
+                        wait
+                        job_count=0
+                fi
+
+        done < ./../../contig_names.txt
+
+        wait
+
+        echo "Fastq files for ont reads done" >> ./../../progress.log
+        print_elapsed_time $start_time_6 >> ./../../progress.log
+
+        cd ..
+        cd ..
 fi
 
-# Check if --blast/-b was provided
-if [ -z "$RUN_BLAST" ]; then
-    echo "Error: --blast/-b argument is required."
-    print_help
-    exit 1
+wait
+
+if [ -n "${RNA_FORWARD}" ]; then
+
+        start_time_7=$(date +%s)
+
+        mkdir rna_fastqs
+        cd samsANDbams
+        mkdir rna_alignments
+        cd rna_alignments
+
+        bwa index ${GENOME}
+        bwa mem -t 4 ${GENOME} ${RNA_FORWARD} ${RNA_REVERSE} > RNAaln.sam
+        samtools view -Sb ./RNAaln.sam -o ./RNAaln.bam
+        samtools sort -o ./RNAaln_sorted.bam ./RNAaln.bam
+        samtools index ./RNAaln_sorted.bam ./RNAaln_sorted.bai
+        bedtools bamtobed -i ./RNAaln_sorted.bam > output.bed
+
+        max_jobs=12
+        job_count=0
+
+        while read -r line; do
+                grep "${line}" output.bed > "${line}"_output.bed &
+
+                job_count=$((job_count + 1))
+
+                if [ "$job_count" -ge "$max_jobs" ]; then
+                        wait
+                        job_count=0
+                fi
+
+        done < ./../../contig_names.txt
+
+        wait
+
+        echo "Bed files for rna reads done" >> ./../../progress.log
+        print_elapsed_time $start_time_7 >> ./../../progress.log
+
+        start_time_8=$(date +%s)
+        while read -r line; do
+                samtools view -L "${line}"_output.bed -h RNAaln_sorted.bam | samtools view -b - > "${line}"_reads.bam &
+
+                job_count=$((job_count + 1))
+
+                if [ "$job_count" -ge "$max_jobs" ]; then
+                        wait
+                        job_count=0
+                fi
+
+        done < ./../../contig_names.txt
+
+        wait
+
+        echo "Bam files for rna reads done" >> ./../../progress.log
+        print_elapsed_time $start_time_8 >> ./../../progress.log
+
+        rm *.bed
+
+
+        start_time_9=$(date +%s)
+        while read -r line; do
+                bedtools bamtofastq -i "${line}"_reads.bam -fq ./../../rna_fastqs/"${line}".fastq &
+
+                job_count=$((job_count + 1))
+
+                if [ "$job_count" -ge "$max_jobs" ]; then
+                        wait
+                        job_count=0
+                fi
+
+        done < ./../../contig_names.txt
+
+        wait
+
+        echo "Fastq files for rna reads done" >> ./../../progress.log
+        print_elapsed_time $start_time_9 >> ./../../progress.log
+
+        cd ..
+        cd ..
 fi
 
-# Check for blast and corresponding argument conditions
-if [ "$RUN_BLAST" = "No" ] && [ -z "$NT" ]; then
-    echo "Error: --nt/-n argument must be given if --blast/-b is set to 'No'."
-    exit 1
-elif [ "$RUN_BLAST" = "Yes" ] && [ -z "$BLAST_OUTPUT" ]; then
-    echo "Error: --blastoutput argument must be given if --blast/-b is set to 'Yes'."
-    exit 1
+wait
+
+if [ -n "${DNA_FORWARD}" ]; then
+
+        start_time_8=$(date +%s)
+
+        mkdir dna_Illumina_fastqs
+        cd samsANDbams
+        mkdir dna_Illumina_alignments
+        cd dna_Illumina_alignments
+
+        bwa index ${GENOME}
+        bwa mem -t 4 ${GENOME} ${DNA_FORWARD} ${DNA_REVERSE} > DNA_Illumina_aln.sam
+        samtools view -Sb ./DNA_Illumina_aln.sam -o ./DNA_Illumina_aln.bam
+        samtools sort -o ./DNA_Illumina_aln_sorted.bam ./DNA_Illumina_aln.bam
+        samtools index ./DNA_Illumina_aln_sorted.bam ./DNA_Illumina_aln_sorted.bai
+        bedtools bamtobed -i ./DNA_Illumina_aln_sorted.bam > output.bed
+
+        max_jobs=12
+        job_count=0
+
+        while read -r line; do
+                grep "${line}" output.bed > "${line}"_output.bed &
+
+                job_count=$((job_count + 1))
+
+                if [ "$job_count" -ge "$max_jobs" ]; then
+                        wait
+                        job_count=0
+                fi
+
+        done < ./../../contig_names.txt
+
+        wait
+
+        echo "Bed files for DNA Illumina reads done" >> ./../../progress.log
+        print_elapsed_time $start_time_8 >> ./../../progress.log
+
+        start_time_9=$(date +%s)
+        while read -r line; do
+                samtools view -L "${line}"_output.bed -h DNA_Illumina_aln_sorted.bam | samtools view -b - > "${line}"_reads.bam &
+
+                job_count=$((job_count + 1))
+
+                if [ "$job_count" -ge "$max_jobs" ]; then
+                        wait
+                        job_count=0
+                fi
+
+        done < ./../../contig_names.txt
+
+        wait
+
+        echo "Bam files for DNA Illumina reads done" >> ./../../progress.log
+        print_elapsed_time $start_time_9 >> ./../../progress.log
+
+        rm *.bed
+
+
+        start_time_10=$(date +%s)
+        while read -r line; do
+                bedtools bamtofastq -i "${line}"_reads.bam -fq ./../../DNA_Illumina_fastqs/"${line}".fastq &
+
+                job_count=$((job_count + 1))
+
+                if [ "$job_count" -ge "$max_jobs" ]; then
+                        wait
+                        job_count=0
+                fi
+
+        done < ./../../contig_names.txt
+
+        wait
+
+        echo "Fastq files for DNA Illumina reads done" >> ./../../progress.log
+        print_elapsed_time $start_time_10 >> ./../../progress.log
+
+        cd ..
+        cd ..
 fi
 
-
-#########################
-#########################
-########RUN_BLAST########
-#########################
-#########################
-
-if [ "$RUN_BLAST" = "No" ]; then
-    sbatch sidrblash.sh --genome $GENOME --nt $NT
-
-
-###################################
-###################################
-########GENERATE_ALIGNMENTS########
-###################################
-###################################
-
-sbatch temp.sh
-
-
-##############################
-##############################
-########GENERATE_STATS########
-##############################
-##############################
-
-#temp.sh will begin kmers.sh, depth.sh, and readGC.sh
-
-#calculate GC content for reads over contig region
-bash contig_GC.sh
-
-#plus and minus strand counts
-bash plusANDminusCounts.sh
-
-
-#various forms of coverage
-bash coverage.sh
-
-
-##########################
-##########################
-########MAKE_TABLE########
-##########################
-##########################
-
-cd stats
-rm *coverage.txt
-
-
-ls *.txt > list
-join --check-order --header -t$'\t' blastIDs.txt Ref_GC.txt > newfile
-sed -i '/Ref_GC/Id' list
-sed -i '/blastIDs/Id' list
-
-while read -r file; do
-    join --check-order --header -t$'\t' newfile "${file}" > newfile.temp
-    mv newfile.temp newfile
-done < list
-
-rm list
-mv newfile SIDRstats.tsv
-
-
-########################
-########################
-########RUN_SIDR########
-########################
-########################
-
-python xgboost.py
-
-
-################################
-################################
-#######MAKE_OUTPUT_FILES########
-################################
-################################
-
-#make a new directory
-mkdir test
-# copy fasta and deny/allow lists into their own folder
-cp ${GENOME} test/
-cp keptContigs.csv  test/
-cp contaminantContigs.csv  test/
-cd test
-
-## Splits contigs into individual files
-awk '/^>/ {OUT=substr($0,2,14) ".fasta"}; OUT {print >OUT}' *.fa
-
-##remove original fasta file
-rm *.fa
-
-##fix file
-awk -F ',' '{print $2}' contaminantContigs.csv | sed 's/"//g' > contaminantlist.txt
-
-awk -F ',' '{print $2}' keptContigs.csv | sed 's/"//g' > keptlist.txt
-
-## combines files that match the allow list into a new “keptcontigs” file
-cat keptlist.txt  | while read line; do cat ""$line".fasta" ; done > keptContigs.fasta
-
-## combines file that match the deny list into a new “contaminant contigs” file
-cat contaminantlist.txt  | while read line; do cat ""$line".fasta" ; done > contaminantContigs2.fasta
-
-
-## move your kept contigs and contaminant contigs files out of the directory. and delete the split out contigs.
-mv contaminantContigs.fasta ./../.
-mv keptContigs.fasta ./../.
-rm -r test
+wait
 
 
 
 
+samsANDbams_end=$(date +%s)
+samsANDbams_runtime=$((samsANDbams_end - samsANDbams_start))
+samsANDbams_runtime_minutes=$((samsANDbams_runtime / 60))
+echo -e "bed, bam, and fastq files generated in $samsANDbams_runtime_minutes minutes /n" >> progress.log
 
+sbatch sidrblast.sh
+sbatch read_GC.sh
+sbatch kmers.sh
+sbatch depth.sh
+sbatch coverage.sh
+sbatch plusANDminus.sh
+sbatch contig_GC.sh
 
-
-
-
-
-
-
-
-
+#potential future parallelization because all these scripts could run at the same time, but I think it is currently messing it up
